50fc121c4de4ba1d7c813f2b9b5890a2fe460127
Your Name <you@example.com>
2024-03-26 21:47:14 +0600

    first commit

--- prothom_yalo/settings.py
+++ prothom_yalo/settings.py
@@ -127,7 +127,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-print(STATIC_ROOT)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
